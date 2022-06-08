f6be34169d2926465fb98a66c4f4614c0644875a
samkleo-book <samkleo@gmail.com>
2022-06-08 09:14:14 +0500

    edit Choice.java

--- src/main/java/hexlet/code/Choice.java
+++ src/main/java/hexlet/code/Choice.java
@@ -2,17 +2,20 @@ package hexlet.code;
 import java.util.Scanner;
 public class Choice {
     public static void firstChoice() {
+        System.out.print("Your choice: ");
         Scanner sc = new Scanner(System.in);
-        int choiceNumber = sc.nextInt();
-        if (choiceNumber == 1) {
-            System.out.println("Your choice: 1\n");
-            System.out.println("Welcome to the Brain Games!\nMay I have your name?");
-            Scanner sc1 = new Scanner(System.in);
-            String name = sc1.next();
-            System.out.println("Hello, " + name + "!");
-        } else if (choiceNumber == 2) {
-            System.out.println("Your choice: 2\n");
-            Even.compare();
+        if (sc.hasNextInt()) {
+            int choiceNumber = sc.nextInt();
+            if (choiceNumber == 1) {
+                System.out.println("Welcome to the Brain Games!\nMay I have your name?");
+                Scanner sc1 = new Scanner(System.in);
+                String name = sc1.next();
+                System.out.println("Hello, " + name + "!");
+            } else if (choiceNumber == 2) {
+                Even.compare();
+            }
+        } else {
+            System.out.print("");
         }
     }
 }
